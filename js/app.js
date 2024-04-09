7bff5bbcb8640c18461fae403c62651e6aacd7ad
Gabriel Riveros <velo02.gr@gmail.com>
2024-04-09 18:26:31 -0400

    Agregar seccion skills e interaccion con javascript

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 const checkbox = document.querySelector('#checkbox');
 const menu = document.querySelector('.header__label');
 const nav = document.querySelector('.nav');
-
-console.log(checkbox);
-console.log(menu);
+const lists = document.querySelectorAll('.nav__list');
 
 const animationMenu = function(){
-    console.log(checkbox.checked)
     nav.classList.toggle('unset');
+    
+    removeAnimation(0);
+        
+}
+
+const setCheck = function(){
+    if(checkbox.checked){
+        checkbox.checked = false;
+        nav.classList.remove('unset');
+        removeAnimation(0);
+    }
 }
 
 checkbox.addEventListener("click", animationMenu);
+
+lists.forEach(
+    item => {
+        item.addEventListener('click', setCheck);
+    }
+)
+
+const removeAnimation = function(count){
+    lists.forEach(
+        item => {
+            item.classList.toggle(`animation${count}`)
+            count++;
+        }
+    )
+}
