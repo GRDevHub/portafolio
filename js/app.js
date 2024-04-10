e4e678eb2a903fc43c91c8873099fb56215b06a0
Gabriel Riveros <velo02.gr@gmail.com>
2024-04-10 16:38:19 -0400

    Agregar modo dark-light

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+// Dark - Light Mode Start
+const storageTheme = localStorage.getItem('theme');
+const systemColorSchema = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' :' light';
+
+const newTheme = storageTheme ?? systemColorSchema;
+
+document.documentElement.setAttribute('data-theme', newTheme);
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const switcherTheme = document.querySelector(".mode__input");
+    const root = document.documentElement;
+
+    if (root.getAttribute('data-theme') === 'dark') {
+        switcherTheme.checked = true;
+    }
+
+    switcherTheme.addEventListener('click', toggleTheme)
+
+    function toggleTheme(){
+        const setTheme = switcherTheme.checked ? 'dark' : 'light';
+
+        root.setAttribute('data-theme', setTheme);
+        // Guardar valor del navegador para que al recargar no cambie los efectos
+        localStorage.setItem('theme', setTheme);
+    }
+
+    
+})
+// Dark - Light Mode end
+
 const checkbox = document.querySelector('#checkbox');
 const menu = document.querySelector('.header__label');
 const nav = document.querySelector('.nav');
